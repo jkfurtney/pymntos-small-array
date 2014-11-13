83c9d2d8f42d44e684432302408a284fd9d8e24e
Jason Furtney <jkfurtney@gmail.com>
2014-11-13 08:25:49 -0600

    adding gh-pages publish script and readme

---
+++ publish.sh
@@ -0,0 +1,6 @@
+cp small-array.html index2.html
+git checkout gh-pages
+cp index2.html index.html
+git commit index.html -m "page update"
+git push origin gh-pages --force
+git checkout master
---
+++ readme.md
@@ -0,0 +1,3 @@
+PyMNtos lightning talk on small arrays
+
+http://jkfurtney.github.io/small-array
